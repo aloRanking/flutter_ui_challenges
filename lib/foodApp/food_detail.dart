a50b61d35cf4a0797bdce68c251608db56f03e3c
aloRanking <adigunlateef02@gmail.com>
2020-03-29 20:05:54 +0100

    added food detail page and food model

---
+++ lib/foodApp/food_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodDetail extends StatefulWidget {
+  @override
+  _FoodDetailState createState() => _FoodDetailState();
+}
+
+class _FoodDetailState extends State<FoodDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+}
