014b10a4bd1b6f80dcc39eb2736352b4f3dfc61c
aloRanking <adigunlateef02@gmail.com>
2020-03-29 20:56:01 +0100

    created detailpage with just food pic

--- lib/foodApp/food_detail.dart
+++ lib/foodApp/food_detail.dart
@@ -1,6 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/foodApp/food_model.dart';
+
+class FoodDetailPage extends StatefulWidget {
+
+  @override
+  _FoodDetailPageState createState() => _FoodDetailPageState();
+}
+
+class _FoodDetailPageState extends State<FoodDetailPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: FoodDetail(),
+      ),
+    );
+  }
+}
+
+
 
 class FoodDetail extends StatefulWidget {
+  final Foood food;
+
+  FoodDetail({this.food});
   @override
   _FoodDetailState createState() => _FoodDetailState();
 }
@@ -8,8 +33,18 @@ class FoodDetail extends StatefulWidget {
 class _FoodDetailState extends State<FoodDetail> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 350.0,
+          decoration: BoxDecoration(
+            color:Color(0xFFFFCA60),
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          child: Image.network(widget.food.imageLink),
+        ),
+      ],
+    );
   }
 }
 
-}
