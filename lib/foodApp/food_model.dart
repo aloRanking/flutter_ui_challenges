4f750f509459fc7602cefd8d577d85f1cfce3a15
aloRanking <adigunlateef02@gmail.com>
2020-04-01 21:44:36 +0100

    added new non veg model

--- lib/foodApp/food_model.dart
+++ lib/foodApp/food_model.dart
@@ -4,9 +4,10 @@ class Foood{
   final String imageLink;
   final String foodName;
   final String foodPrice;
+  final List<String> smallImages;
 
 
- const Foood({this.id,this.imageLink, this.foodName, this.foodPrice});
+ const Foood({this.id,this.imageLink, this.foodName, this.foodPrice, this.smallImages});
 
 
 }
@@ -18,26 +19,62 @@ List<Foood> foods = [
     imageLink: 'https://www.vegetariantimes.com/.image/t_share/MTQ3MDM3MzQ5NjA2MzM2NDA3/zi3000-shutterstock-buddha-bowl.jpg',
     foodName: 'Vegetarian Dinner',
     foodPrice: '\$0.60',
+   smallImages: [
+     'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+     'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+     'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+     'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+     'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+
+
+   ]
   ),
   const Foood(
     id: '2',
     imageLink: 'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
     foodName: 'Vegetarian Dinner',
     foodPrice: '\$0.60',
+      smallImages: [
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+
+
+      ]
   ),
 
   const Foood(
     id: '3',
     imageLink: 'https://static.toiimg.com/photo/54327253/.jpg',
     foodName: 'Vegetarian Dinner',
-    foodPrice: '\$0.60',
+    foodPrice: '\$02.0',
+      smallImages: [
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+
+
+      ]
   ),
 
   const Foood(
     id: '4',
     imageLink: 'https://images.immediate.co.uk/production/volatile/sites/2/2016/08/25471.jpg?quality=90&resize=620,413',
     foodName: 'Vegetarian Dinner',
-    foodPrice: '\$0.60',
+    foodPrice: '\$1.0',
+      smallImages: [
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+        'https://assets3.thrillist.com/v1/image/2856117/size/gn-gift_guide_variable_c.jpg',
+
+
+      ]
   ),
 
 
