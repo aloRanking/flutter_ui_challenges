dc6050d8d5e938538e01b697fc029e0239f35be4
aloRanking <adigunlateef02@gmail.com>
2020-04-10 18:13:58 +0100

    discarded some changes

--- lib/covidApp/covid_model.dart
+++ lib/covidApp/covid_model.dart
@@ -1,59 +1,34 @@
-
 import 'package:flutter_ui_challenges/covidApp/services/networking.dart';
-
 
 const covidLiveStat = 'https://covid-19-live-stats.p.rapidapi.com/';
 
 const covidAPI = 'https://covid-19-data.p.rapidapi.com/';
 
-
-
-class Covid{
-
+class Covid {
   String confirmed;
   String recovered;
   String critical;
   String death;
 
-
-
-
-
-
-
-
-  Future<dynamic> getCovidTotalResult() async{
-
-    var url = covidAPI +'totals?format=undefined';
+  Future<dynamic> getCovidTotalResult() async {
+    var url = covidAPI + 'totals?format=undefined';
 
     NetworkHelper networkHelper = NetworkHelper('$url');
 
     var covidData = await networkHelper.getData();
-
 
     print(covidData);
     return covidData;
-
-
   }
 
   Future<dynamic> getCovidCountryResult(String country) async {
-
-    var url = covidAPI +'country?format=undefined&name=$country';
+    var url = covidAPI + 'country?format=undefined&name=$country';
 
     NetworkHelper networkHelper = NetworkHelper('$url');
     var covidData = await networkHelper.getData();
 
-
-
-
     return covidData;
-
   }
 
-  void getCovidData(){
-
-
-
-  }
+  void getCovidData() {}
 }
--- lib/covidApp/services/networking.dart
+++ lib/covidApp/services/networking.dart
@@ -1,54 +1,55 @@
-
 import 'dart:convert';
-
 
 import 'package:http/http.dart' as http;
 
-class NetworkHelper{
-
+class NetworkHelper {
   String url;
   String host = "covid-19-data.p.rapidapi.com";
   String _ApiKey = '083b4276f6mshf879b92d4592dcdp16b4c2jsn1c6468331975';
 
   NetworkHelper(this.url);
 
-  Future getData() async{
-
-    http.Response response = await http.get(url, headers: {"x-rapidapi-host": host, "x-rapidapi-key": _ApiKey, });
+  Future getData() async {
+    http.Response response = await http.get(url, headers: {
+      "x-rapidapi-host": host,
+      "x-rapidapi-key": _ApiKey,
+    });
 
     try {
-      if(response.statusCode == 200){
+      if (response.statusCode == 200) {
         String data = response.body;
 
         print(data);
         return jsonDecode(data);
-      }else
+      } else
         print(response.statusCode);
     } on Exception catch (e) {
       // TODO
       print(e);
     }
-
   }
 
-  Future postData() async{
-
+  Future postData() async {
     //http.Response response = await http.get(url, headers: {"x-rapidapi-host": host, "x-rapidapi-key": _ApiKey, });
 
-    http.Response response = await http.post(url, headers: {"x-rapidapi-host": host, "x-rapidapi-key": _ApiKey, },);
-
+    http.Response response = await http.post(
+      url,
+      headers: {
+        "x-rapidapi-host": host,
+        "x-rapidapi-key": _ApiKey,
+      },
+    );
 
     try {
-      if(response.statusCode == 200){
+      if (response.statusCode == 200) {
         String data = response.body;
 
         return jsonDecode(data);
-      }else
+      } else
         print(response.statusCode);
     } on Exception catch (e) {
       // TODO
       print(e);
     }
-
   }
 }
